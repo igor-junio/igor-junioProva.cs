b7168a5e05da59d942ed3bcd592acc564ea16f94
igor-junioprova.cs <igorcarlosdasilvasilva3@gmail.com>
2026-09-14 14:01:49 -0300

    Update prova de .cs

--- prova de .cs
+++ prova de .cs
@@ -1,11 +1,13 @@
-//o que deve ser feito?
-//Desenvover um sistema simples contendo: 3 clase difente , e o codico organizado , e o uso correto de poo.
-//cada clase deve funcionar de forma independente.
-//
-//
+// o que deve ser feito?
+// Desenvover um sistema simples contendo: 3 clase difente , e o codico organizado , e o uso correto de poo.
+// cada clase deve funcionar de forma independente.
 
-
-
+// Requisitos do sistema. o sistema deve conter:
+// 3 clase (Ex: )
+// 
+// 
+// 
+// 
 class
 
 
