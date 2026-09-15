0b05607689c758f6087062fd452b8da7c263ccb4
igor-junio <igorcarlosdasilvasilva3@gmail.com>
2026-09-14 22:34:33 -0300

    Update prova de .cs

--- prova de .cs
+++ prova de .cs
@@ -69,3 +69,51 @@ public Pessoa(string nome, int idade)
     }
 }
 
+public override string ToString()
+    {
+        return $"Nome: {Nome} | Idade: {Idade}";
+    }
+}
+
+class Produto
+{
+    private string nome;
+    private double preco;
+
+    public string Nome
+    {
+        get { return nome; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new Exception("O nome do produto não pode ser vazio.");
+
+            nome = value;
+        }
+    }
+    
+public double Preco
+    {
+        get { return preco; }
+        set
+        {
+            if (value < 0)
+                throw new Exception("O preço não pode ser negativo.");
+
+            preco = value;
+        }
+    }
+
+public Produto(string nome, double preco)
+    {
+        Nome = nome;
+        Preco = preco;
+    }
+
+public override string ToString()
+    {
+        return $"Produto: {Nome} | Preço: R$ {Preco:F2}";
+    }
+}
+
+
